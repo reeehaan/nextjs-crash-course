5dd351e6149d1e9125f39d557998d4d2c28c7c58
reeehaan <rehan221104@gmail.com>
2026-04-01 21:38:25 +0530

    feat : add event details like title, image, slug, locations, etc.

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,24 +1,39 @@
-'use client';
-import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
-import posthog from 'posthog-js';
 
 interface Props {
-    title: string
-    image: string
+    title: string;
+    image: string;
+    slug: string;
+    location: string;
+    date: string;
+    time: string;
 }
 
-const EventCard = ({title, image} : Props) => {
-    const handleClick = () => {
-        posthog.capture('event_card_clicked', { event_title: title });
-    };
-
+const EventCard = ({ title, image, slug, location, date, time }: Props) => {
     return (
-      <Link href={`/events}`} id="event-card" onClick={handleClick}>
-        <Image src={image} alt={title} width={400} height={300} className="poster" />
-          <p className="title">{title}</p>
-      </Link>
+        <Link href={`/events/${slug}`} id="event-card">
+            <Image src={image} alt={title} width={410} height={300} className="poster" />
+
+            <div className="flex flex-row gap-2">
+                <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+                <p>{location}</p>
+            </div>
+
+            <p className="title">{title}</p>
+
+            <div className="datetime">
+                <div>
+                    <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+                    <p>{date}</p>
+                </div>
+                <div>
+                    <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+                    <p>{time}</p>
+                </div>
+            </div>
+        </Link>
     )
 }
+
 export default EventCard
