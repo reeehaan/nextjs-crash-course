6046e60ab86edade2717ffbcc202cd88d5482e11
reeehaan <rehan221104@gmail.com>
2026-04-07 14:56:13 +0530

    feat: "Add BookEvent form component

---
+++ components/BookEvent.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React, {useState} from 'react'
+
+const BookEvent = () => {
+
+    const [email, setEmail] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        setTimeout(()=>{
+            setSubmitted(true);
+        },1000);
+    }
+    return (
+        <div id="book-event">
+            {submitted ? (
+                <p className="text-sm">Thank you for signing up!</p>
+            ):(
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        <input
+                            id="email"
+                            value={email}
+                            type="email"
+                            placeholder="Enter your email address"
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+
+                    <button type="submit" className="buttom-submit"> Submit</button>
+                </form>
+            )}
+        </div>
+    )
+}
+export default BookEvent
